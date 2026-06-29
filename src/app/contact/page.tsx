2cc0aafef4a993ca3321d6125cae04231b726582
evonix-rushikeshs <rushikesh.s@evonix.co>
2026-06-29 19:06:57 +0530

    updated code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -73,7 +73,7 @@ const ContactPage = () => {
               title="IMSCDR Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.3476661080676!2d74.742185074398!3d19.09239805145913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdcb0fa893e7c8d%3A0xcd1d5bf50be28400!2sInstitute%20of%20Management%20Studies%20Career%20Development%20%26%20Research!5e0!3m2!1sen!2sin!4v1782409649936!5m2!1sen!2sin"
               width="100%"
-              height="450"
+              height="540"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
