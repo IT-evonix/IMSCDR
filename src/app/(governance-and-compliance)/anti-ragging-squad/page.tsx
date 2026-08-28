b09c9d8c0c296f64a264ef76735c6d788c142ffa
Swapnil Kakade <kakade.swapnil@evonix.co>
2026-08-28 12:49:08 +0530

    mystable code aug 28-morning

--- src/app/(governance-and-compliance)/anti-ragging-squad/page.tsx
+++ src/app/(governance-and-compliance)/anti-ragging-squad/page.tsx
@@ -12,11 +12,11 @@ export default function GovernanceGoverningBody() {
         title="Anti-Ragging Squad"
         breadcrumbs={[{ label: "Anti-Ragging Squad" }]}
       />
-      <div className="fullwidth_page test">
+      <div className="fullwidth_page">
         <div className="tablemain_section">
         <div className="container">
           <div className="mb-4">
-            <div className="heading text-center">Committee Members </div>
+            <div className="heading text-center">Committee Members</div>
           </div>
           <Table columns={antiRaggingSqualColumns} data={antiRaggingSqualMembers} />
         </div>
